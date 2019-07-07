29b2d19c7ed9295b8ed490d94436ac86bc395aa6
Mikhail Fursov <mike.fursov@gmail.com>
2019-07-07 17:17:11 -0400

    Fixing maven plugins & java doc tags so the project can be built using all JDKs (>=7, <=12) with no issues.

--- src/main/java/com/github/openjson/JSONObject.java
+++ src/main/java/com/github/openjson/JSONObject.java
@@ -48,7 +48,7 @@ import java.util.TreeMap;
  * be coerced using {@link Number#intValue() intValue}. Strings
  * that can be coerced using {@link Double#valueOf(String)} will be,
  * and then cast to int.
- * <li><a name="lossy">When the requested type is a long, other {@link Number} types will
+ * <li><a id="lossy">When the requested type is a long, other {@link Number} types will
  * be coerced using {@link Number#longValue() longValue}. Strings
  * that can be coerced using {@link Double#valueOf(String)} will be,
  * and then cast to long. This two-step conversion is lossy for very
